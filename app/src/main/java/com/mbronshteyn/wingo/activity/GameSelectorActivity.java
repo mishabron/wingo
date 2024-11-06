ec1539fdac52f4c36d6961c971df2edc853ba179
misha <misha.bronshteyn@gmail.com>
2024-11-05 23:03:58 -0500

    added more sounds

--- app/src/main/java/com/mbronshteyn/wingo/activity/GameSelectorActivity.java
+++ app/src/main/java/com/mbronshteyn/wingo/activity/GameSelectorActivity.java
@@ -33,6 +33,7 @@ public class GameSelectorActivity extends WingoActivity {
         button6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                stopPlaySound(R.raw.select_bet_spanish);
                 Intent intent = new Intent(getApplicationContext(), GameActivity.class);
                 Bundle bundle = ActivityOptionsCompat.makeCustomAnimation(activity, R.anim.fade_in, R.anim.fade_out).toBundle();
                 startActivity(intent, bundle);
